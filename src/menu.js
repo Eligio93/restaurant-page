11c141bdab3fe005e9a94c493f59ea704c1e93ce
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-14 14:33:26 +1100

    add cocktails section and remove button style

--- src/menu.js
+++ src/menu.js
@@ -10,12 +10,12 @@ const menu=[{
 },{
     id:"Sides",
     dish:["Fries","Roasted Potatoes","Sweet Potatoes"]
+},{
+    id:"Cocktails",
+    dish:["Negroni","Old Fashioned","Mojito","Mocktail"]
 }]
 
 const createMenu=function(){
-    let menuButton=document.getElementById("menu");
-    menuButton.style.borderTop="2px solid var(--secondary-color)";
-    menuButton.style.borderBottom="2px solid var(--secondary-color)";
     let content=document.getElementById("content");
     let menuPage=document.createElement("div");
     menuPage.id="menu-page";
