04269e76a0f70b738fcec082145922b0ae17db6a
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-14 14:34:04 +1100

    add style to the selected button with a class

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ let menuButton=document.getElementById("menu");
 let contactsButton=document.getElementById("contacts");
 let content=document.getElementById("content");
 createHome();
+homeButton.className="active";
 homeButton.addEventListener("click",function(){
-    
+    homeButton.className="active";
+    menuButton.className="inactive";
+    contactsButton.className="inactive"
     content.innerHTML="";
     createHome();
 });
 menuButton.addEventListener("click",function(){
+    homeButton.className="inactive";
+    menuButton.className="active";
+    contactsButton.className="inactive"
    
     content.innerHTML="";
    createMenu();
