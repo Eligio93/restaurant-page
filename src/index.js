031cacd8d63b7394d73840c797b86490b5db550b
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-14 13:00:24 +1100

    add Event Listener to the nav buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import createHome from "./home";
-
-function createContent(){
+import createMenu from "./menu";
+let homeButton=document.getElementById("home");
+let menuButton=document.getElementById("menu");
+let contactsButton=document.getElementById("contacts");
+let content=document.getElementById("content");
+createHome();
+homeButton.addEventListener("click",function(){
+    
+    content.innerHTML="";
     createHome();
-}
-createContent();
+});
+menuButton.addEventListener("click",function(){
+   
+    content.innerHTML="";
+   createMenu();
+})
+
