9878444d7aacd355537b312014d0698d72d06877
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-15 12:59:51 +1100

    forrmats and clean code

--- src/menu.js
+++ src/menu.js
@@ -1,35 +1,33 @@
-const menu=[{
-    id:"Entree",
-    dish:["Charcuterie Board","Mini Tapas Selection","Mixed Entree"]
-},{
-    id:"Mains",
-    dish:["Pork Ribs","300gr Angus Steak","Chicken Club Sandwich","Wagyu Burger","Margherita Pizza"]
-},{
-    id:"Desserts",
-    dish:["Brownies","Tiramisu","Cheesecake"]
-},{
-    id:"Sides",
-    dish:["Fries","Roasted Potatoes","Sweet Potatoes"]
-},{
-    id:"Cocktails",
-    dish:["Negroni","Old Fashioned","Mojito","Mocktail"]
+const menu = [{
+    id: "Entree",
+    dish: ["Charcuterie Board", "Mini Tapas Selection", "Mixed Entree"]
+}, {
+    id: "Mains",
+    dish: ["Pork Ribs", "300gr Angus Steak", "Chicken Club Sandwich", "Wagyu Burger", "Margherita Pizza"]
+}, {
+    id: "Desserts",
+    dish: ["Brownies", "Tiramisu", "Cheesecake"]
+}, {
+    id: "Sides",
+    dish: ["Fries", "Roasted Potatoes", "Sweet Potatoes"]
+}, {
+    id: "Cocktails",
+    dish: ["Negroni", "Old Fashioned", "Mojito", "Mocktail"]
 }]
 
-const createMenu=function(){
-    let content=document.getElementById("content");
-    let menuPage=document.createElement("div");
-    menuPage.id="menu-page";
-    for (let i=0;i<menu.length;i++){
-        let menuHeader=document.createElement("div");
-        menuHeader.className="menu-header";
-        let title=document.createElement("h3");
-        title.textContent=menu[i].id;
-        menuHeader.appendChild(title);
+const createMenu = function () {
+    let content = document.getElementById("content");
+    let menuPage = document.createElement("div");
+    menuPage.id = "menu-page";
+    for (let i = 0; i < menu.length; i++) {
+        let menuHeader = document.createElement("h3");
+        menuHeader.className = "menu-header";
+        menuHeader.textContent = menu[i].id;
         menuPage.appendChild(menuHeader);
-        let unList=document.createElement("ul");
-        for(let j=0;j<menu[i].dish.length;j++){
-            let li=document.createElement("li");
-            li.textContent=menu[i].dish[j];
+        let unList = document.createElement("ul");
+        for (let j = 0; j < menu[i].dish.length; j++) {
+            let li = document.createElement("li");
+            li.textContent = menu[i].dish[j];
             unList.appendChild(li);
             menuPage.appendChild(unList);
 
