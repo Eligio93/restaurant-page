d4c8db2227d01d7e68e2d678bb9810667d2ff048
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-15 12:03:22 +1100

    makes the code more standard

--- src/home.js
+++ src/home.js
@@ -1,11 +1,11 @@
 const createHome=function(){
     let content=document.getElementById("content");
-    let homeBackground=document.createElement("div");
-    homeBackground.id="home-background";
+    let homePage=document.createElement("div");
+   homePage.id="home-page";
     let homeText=document.createElement("h2");
     homeText.textContent="Welcome to Truth Mixology and Kitchen, where every sip and bite tells a story.Discover the perfect blend of handcrafted cocktails and delectable dishes that tantalize your taste buds. Join us on a journey of flavors, where authenticity and creativity meet in perfect harmony."
-    homeBackground.appendChild(homeText);
-    content.appendChild(homeBackground);
+    homePage.appendChild(homeText);
+    content.appendChild(homePage);
 }
 
 export default createHome;
