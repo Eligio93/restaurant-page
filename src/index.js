b9953dee1c8605c566249697ea426d3710dee45c
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-15 14:25:03 +1100

    import git icon and append to footer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import createHome from "./home";
 import createMenu from "./menu";
 import createContacts from "./contacts";
 import "./style.css";
+import gitIcon from "./gitlogo.svg";
 
 let homeButton=document.getElementById("home");
 let menuButton=document.getElementById("menu");
 let contactsButton=document.getElementById("contacts");
 let content=document.getElementById("content");
+document.getElementById("git-logo").src=gitIcon;
 createHome();
 homeButton.className="active";
 homeButton.addEventListener("click",function(){
