407704897598a45c78646f90bf2209d08dfedc2f
Eligio93 <eligio.cristantielli@gmail.com>
2023-10-15 12:02:50 +1100

    improves code in contacts creating object

--- src/contacts.js
+++ src/contacts.js
@@ -1,32 +1,35 @@
+let contacts=[{
+    header:"Phone",
+    info:["Tel. +39 080 698 3621"]
+},{
+    header:"Hours",
+    info:["Wed-Mon: 18:00 - 2:00","Tue: Closed"]
+},{
+    header:"Where to find us",
+    info:["Via S. Giacomo, 34, 70014 Conversano BA, Italy"]
+}]
+
 const createContacts=function(){
     let content=document.getElementById("content");
     let contactsPage=document.createElement("div");
     contactsPage.id="contacts-page";
-    let phoneHeader=document.createElement("h3");
-    phoneHeader.className="contact-header";
-    phoneHeader.textContent="Phone";
-    contactsPage.appendChild(phoneHeader);
-    let phoneNumber=document.createElement("p");
-    phoneNumber.textContent="Tel. +39 080 698 3621";
-    contactsPage.appendChild(phoneNumber);
-    let hoursHeader=document.createElement("h3");
-    hoursHeader.className="contact-header";
-    hoursHeader.textContent="Hours";
-    contactsPage.appendChild(hoursHeader);
-    let hours=document.createElement("p");
-    hours.innerHTML="Wed-Mon: 18:00 - 2:00<br>Tue: Closed"
-    contactsPage.appendChild(hours);
-    let whereHeader=document.createElement("h3");
-    whereHeader.className="contact-header";
-    whereHeader.textContent="Where to find Us";
-    contactsPage.appendChild(whereHeader);
-    let map=document.createElement("img")
-    map.src="/src/Location.png";
-    contactsPage.appendChild(map);
-    let address=document.createElement("p");
-    address.textContent="Via S. Giacomo, 34, 70014 Conversano BA, Italy";
-    contactsPage.appendChild(address);
+    for(let i=0;i<contacts.length;i++){
+        let contactHeader=document.createElement("h3");
+        contactHeader.className="contact-header";
+        contactHeader.textContent=contacts[i].header;
+        contactsPage.appendChild(contactHeader);
+        if(contacts[i].header=="Where to find us"){
+            let map=document.createElement("img");
+            map.src="/src/Location.png";
+            contactsPage.appendChild(map);
+        }
+        for(let j=0;j<contacts[i].info.length;j++){
+            let contactInfo=document.createElement("p");
+            contactInfo.textContent=contacts[i].info[j];
+            contactsPage.appendChild(contactInfo);
+        }
+       
+    }
     content.appendChild(contactsPage);
-
 }
 export default createContacts;
